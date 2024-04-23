5178e20af529399412e44ca1eb883e4f132841b1
Angel Vazquez <angelvazquez470@gmail.com>
2024-04-23 09:19:09 -0600

    commit before pulling changes

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'payment_method',
+        'amount',
+        'next_payment',
+        'suscription_id'
+    ];  
+
+    protected $casts = [
+        'next_payment' => 'date'
+    ];
+
+    //relationships
+    public function suscription() :BelongsTo
+    {
+        return $this->belongsTo(Suscription::class);
+    }
 }
