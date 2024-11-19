50570b00cbb47d3427f1bef53f90a35e47efa5b2
Miguel Vázquez <miguelvz26.mv@gmail.com>
2024-11-18 18:42:28 -0600

    Reporte de servicio
    • no guardaba ni editaba descripcion de servicio
    • Agregado .padStart(4, '0') en folios de varias tablas

--- resources/js/Components/MyComponents/Service/ServicesTable.vue
+++ resources/js/Components/MyComponents/Service/ServicesTable.vue
@@ -15,7 +15,7 @@
                 <tr @click="$inertia.visit(route('services.show', encodeId(service.id)))"
                     v-for="(service, index) in services" :key="index"
                     class="*:text-xs *:py-2 *:px-4 hover:bg-primarylight cursor-pointer">
-                    <td class="rounded-s-full">{{ 'S-' + service.folio }}</td>
+                    <td class="rounded-s-full">{{ 'S-' + String(service.folio).padStart(4, '0') }}</td>
                     <td>{{ service.name }}</td>
                     <td>{{ service.category ?? '-' }}</td>
                     <td>${{ service.price?.toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",") ?? '-' }}</td>
