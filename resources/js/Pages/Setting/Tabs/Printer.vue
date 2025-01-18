aca61c94696a87f8fc8cf0c201cf463613920878
Angel Vazquez <angelvazquez470@gmail.com>
2025-01-18 12:28:08 -0600

    varias modificaciones

--- resources/js/Pages/Setting/Tabs/Printer.vue
+++ resources/js/Pages/Setting/Tabs/Printer.vue
@@ -1,8 +1,24 @@
 <template>
-    <section class="my-5 space-y-3 md:space-y-0 lg:grid grid-cols-2 gap-3">
+    <section class="my-5">
+        <article class="border border-grayD9 rounded-t-md p-4">
+            <div class="flex items-center space-x-2">
+                <span class="text-base font-bold">Conexión por bluetooth</span>
+                <div class="bg-[#CCD8FD] rounded-full py-1 inline-block">
+                    <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <mask id="mask0_15673_25" style="mask-type:alpha" maskUnits="userSpaceOnUse" x="0" y="0" width="14" height="14">
+                            <rect width="14" height="14" fill="#D9D9D9"/>
+                        </mask>
+                        <g mask="url(#mask0_15673_25)">
+                            <path d="M6.41406 12.8327V8.39935L3.73073 11.0827L2.91406 10.266L6.18073 6.99935L2.91406 3.73268L3.73073 2.91602L6.41406 5.59935V1.16602H6.9974L10.3224 4.49102L7.81406 6.99935L10.3224 9.50768L6.9974 12.8327H6.41406ZM7.58073 5.59935L8.68906 4.49102L7.58073 3.41185V5.59935ZM7.58073 10.5868L8.68906 9.50768L7.58073 8.39935V10.5868Z" fill="#082FAF"/>
+                        </g>
+                    </svg>
+                </div>
+            </div>
+            <p class="text-[#575757] text-sm mt-2">Para poder conectar tu impresora de tickets a tu sistema de punto de venta de manera inalámbrica, asegúrate de encender el Bluetooth en el dispositivo desde el que deseas realizar la conexión (ya sea una computadora o un teléfono móvil). Esto permitirá que el dispositivo detecte la impresora y puedas establecer la conexión correctamente utilizando el Código UUID de servicio y el Código UUID characteristic.</p>
+        </article>
         <!-- UUID SERVICE ----------------------------------------->
         <!-- --------------------------------------------------------- -->
-        <article class="border border-grayD9 rounded-md p-4 relative">
+        <article class="border border-grayD9 p-4 relative">
         <i v-if="!editUUIDService" @click="editUUIDService = true" class="fa-solid fa-pen absolute top-3 right-3 text-xs text-primary cursor-pointer bg-gray-100 rounded-full py-1 px-[5px]"></i>
         <h2 class="font-bold">Código UUID Service</h2>
         <p class="text-sm text-gray99 mb-4">Puedes encontrarlo en las especificaciones del fabricante. Si no lo encuentras contactar a soporte de Ezy Ventas</p>
@@ -25,7 +41,7 @@
 
         <!-- UUID Characteristic ----------------------------------------->
         <!-- --------------------------------------------------------- -->
-        <article class="border border-grayD9 rounded-md p-4 relative">
+        <article class="border border-grayD9 p-4 relative">
         <i v-if="!editUUIDCharacteristic" @click="editUUIDCharacteristic = true" class="fa-solid fa-pen absolute top-3 right-3 text-xs text-primary cursor-pointer bg-gray-100 rounded-full py-1 px-[5px]"></i>
         <h2 class="font-bold">Código UUID Characteristic</h2>
         <p class="text-sm text-gray99 mb-4">Puedes encontrarlo en las especificaciones del fabricante. Si no lo encuentras contactar a soporte de Ezy Ventas</p>
@@ -48,7 +64,7 @@
 
         <!-- ACTIVAR IMPRESIÓN AUTOMÁTICA EN PUNTO DE VENTA-------------->
         <!-- --------------------------------------------------------- -->
-        <article class="border border-grayD9 rounded-md p-4 relative">
+        <article class="border border-grayD9 rounded-b-md p-4 relative">
         <h2 class="font-bold">Impresión automática</h2>
         <p class="text-sm text-gray99 mb-4">Imprime el ticket de cada venta realizada automáticamente</p>
         <div class="flex items-center text-sm">
