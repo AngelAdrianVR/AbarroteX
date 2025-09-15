1c4b063e4e668405d96c8db063e463ef37039c78
Miguel Vázquez <miguelvz26.mv@gmail.com>
2025-09-15 11:21:53 -0600

    detalles

--- resources/js/Pages/Setting/Tabs/Printer.vue
+++ resources/js/Pages/Setting/Tabs/Printer.vue
@@ -2,7 +2,7 @@
     <div>
         <p class="text-sm">
             Si aún no tienes el plugin para la impresión de tickets, 
-            <a href="@/../../ezy_plugin_v1.0.0.zip" target="_blank" class="underline text-primary">
+            <a href="@/../../ezy_plugin_v1.0.0.exe" target="_blank" class="underline text-primary">
                 Click aqui (v1.0.0 Windows x64)
             </a>
         </p>
